fb7cd674f9822c3b5f622ec8ec6877e097b6b4f0
axelleonardo-1 <123020940+axelleonardo-1@users.noreply.github.com>
2024-09-05 19:08:03 -0600

    Primer entregable de IoT Axel

---
+++ Practica_1/Codigo/HolaMundo.ino
@@ -0,0 +1,21 @@
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP32     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+
+
+void setup()
+{
+Serial.begin(9600);
+
+}
+
+
+void loop()
+{
+
+    Serial.println("Hola mundo");
+    delay(3000);
+
+}
